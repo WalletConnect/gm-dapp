8bcecad13433e3a6fe61f6e56decb0f6fe0b025d
Cali93 <32299095+Cali93@users.noreply.github.com>
2023-07-11 10:13:37 +0300

    chore(auth-header): added authorization header for Cast /notify req

---
+++ pages/api/notify.ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+if (!projectId) {
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
+}
+
+const projectSecret = process.env.CAST_PROJECT_SECRET;
+if (!projectSecret) {
+  throw new Error("You need to provide CAST_PROJECT_SECRET env variable");
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    throw new ReferenceError("Method not allowed");
+  }
+
+  const notificationPayload = req.body;
+  if (!notificationPayload) {
+    return res.status(400).json({ success: false });
+  }
+
+  try {
+    // We can construct the URL to the Cast server using the `castUrl` property
+    // of the `PushDappClient` (which will be `https://cast.walletconnect.com` by default),
+    // together with our Project ID.
+    const result = await fetch(
+      `https://cast.walletconnect.com/${projectId}/notify`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: projectSecret as string,
+        },
+        body: JSON.stringify(notificationPayload),
+      }
+    );
+
+    const gmRes = await result.json(); // { "sent": ["eip155:1:0xafeb..."], "failed": [], "not_found": [] }
+    const isSuccessfulGm = gmRes.sent.includes(notificationPayload.accounts[0]);
+    return res.status(gmRes.status).json({ success: isSuccessfulGm });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: error?.message ?? "Internal server error",
+    });
+  }
+}
