96c81a0148b27a68f71b27a41d5477f1b8b5c86e
Cali <32299095+Cali93@users.noreply.github.com>
2023-07-14 15:45:19 +0300

    chore(cast-auth): add authorization header and lorem ipsum for notification body  (#13)
    * chore(auth-header): added authorization header for Cast /notify req
    
    * chore(long-message): use lorem ipsum for notification body
    
    * fix(api-url): fix API route for /notify

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,3 +4,7 @@ export const PROJECT_METADATA = {
   name: "gm-dApp",
   url: "https://gm.walletconnect.com",
 };
+
+export const NOTIFICATION_BODY = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia blandit dui non sollicitudin. Duis sollicitudin euismod leo, a venenatis arcu condimentum eget. Duis congue accumsan mi vel malesuada. Proin quis felis tempus, tristique turpis vel, accumsan risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis nisi velit, gravida nec ligula eu, egestas consequat augue. Phasellus dapibus laoreet neque, ut interdum lectus blandit et. Aenean vitae quam quis neque vehicula egestas tempor sit amet orci. Mauris congue malesuada sodales. Aliquam erat volutpat. Donec et ipsum justo.
+
+Aliquam vitae tempus nisl. Nunc id est maximus, semper nunc a, dictum dui. Vivamus varius pharetra nisl id tristique. Duis varius elementum condimentum. Fusce commodo nunc sapien, eget suscipit mauris ultrices id. Aliquam erat volutpat. Donec eu pharetra nunc. Nam ipsum felis, malesuada id tristique sed, bibendum sit amet lorem. Aliquam in nibh nec nibh libero.`;
