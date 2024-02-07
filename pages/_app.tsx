9a3aad70dac21af4ec12cfd24454f0c81942e986
Celine Sarafa <celinesarafa@gmail.com>
2024-02-07 16:06:58 +0100

    chore: add dev timestamp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { theme } from "../styles/theme";
 import Footer from "../components/core/Footer";
 import { PROJECT_METADATA } from "../utils/constants";
 import Head from "next/head";
+import DevTimeStamp from "../components/DevTimeStamp";
 
 // 1. Get projectID at https://cloud.walletconnect.com
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
@@ -56,6 +57,7 @@ function MyApp({ Component, pageProps }: AppProps) {
               </GridItem>
               <Flex justifyContent="center">
                 <GridItem area={"main"} justifyContent="center">
+		  <DevTimeStamp />
                   <Component {...pageProps} />
                 </GridItem>
               </Flex>
